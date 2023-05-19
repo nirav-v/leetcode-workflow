9b2609d11b42660a57340fe3dcab3cc5b4ed0489
Nirav Venkatesan <niravvenkatesan@gmail.com>
2023-05-19 19:54:37 +0000

    [LeetCode Sync] Runtime - 69 ms (5.47%), Memory - 41.8 MB (100.00%)

--- solved-problems/2732-counter/solution.js
+++ solved-problems/2732-counter/solution.js
@@ -3,14 +3,12 @@
  * @return {Function} counter
  */
 var createCounter = function(n) {
-    // keep track of calls to inner function so that we do not increment n on the first call
-    let calls = 0;
+    // initialize counter only once in outer function
+    let count = n - 1;
     // inner function body is returned into global memory when outer is called. Maintains referecne to count variable
     return function() {
-        calls++;
-        if (calls === 1) return n;
-        n = n + 1; // increment count during each invocation 
-        return n;
+        count = count + 1; // increment count during each invocation 
+        return count;
     };
 };
 
