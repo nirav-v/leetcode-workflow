ce57192124017833d2204f5fec82a0b59c9e4b66
Nirav Venkatesan <niravvenkatesan@gmail.com>
2023-06-29 01:55:39 +0000

    [LeetCode Sync] Runtime - 76 ms (5.41%), Memory - 43.5 MB (100.00%)

--- solved-problems/2741-function-composition/solution.js
+++ solved-problems/2741-function-composition/solution.js
@@ -8,14 +8,14 @@
 var compose = function(functions) {
 
 	return function(x) {
-    
+        let prevResult = x; //there will be no prevResult on 1st function call so it should use the original value of x
         if (functions.length === 0) return x;
         for (let i = functions.length -1; i >= 0; i--){
         // execute first callback passed to outer function on x, store result 
         // call each subsequent callback on previous result
-            x = functions[i](x);
+            prevResult = functions[i](prevResult);
         }
-        return x;
+        return prevResult;
     }
 };
 
