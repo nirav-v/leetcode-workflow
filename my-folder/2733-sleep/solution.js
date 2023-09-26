a805931c0a8d2fb894a5f054c7abc388771e3351
Nirav Venkatesan <niravvenkatesan@gmail.com>
2023-09-26 22:25:16 +0000

    [LeetCode Sync] Runtime - 63 ms (6.79%), Memory - 41.8 MB (100.00%)

--- my-folder/2733-sleep/solution.js
+++ my-folder/2733-sleep/solution.js
@@ -2,12 +2,12 @@
  * @param {number} millis
  */
  function sleep(millis) {
-    // construct a new promise object that resolves after millis milliseconds
-    let result = new Promise((resolve, reject)=> {
-        setTimeout(()=> resolve('done'), millis)
-    })
-    // return the promise
-    return result;
+ 
+        return new Promise((resolve, reject) =>{
+            setTimeout(()=> resolve("Awake"), millis)
+        })
+ 
+ 
 }
 
 /** 
