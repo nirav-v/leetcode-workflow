8f8022368501a5e1907b144392e511cfd399cbef
Nirav Venkatesan <niravvenkatesan@gmail.com>
2023-10-04 17:32:37 +0000

    [LeetCode Sync] Runtime - 60 ms (13.77%), Memory - 41.8 MB (100.00%)

--- my-folder/2796-allow-one-function-call/solution.js
+++ my-folder/2796-allow-one-function-call/solution.js
@@ -3,14 +3,13 @@
  * @return {Function}
  */
 var once = function(fn) {
-    let calls = 0;
-    return function(...args){
-        if (calls <= 0 ){
-            calls++;
-            return fn(...args);
-        }
-        else {
-            return undefined;
+    
+    let called = false;
+    
+	return function(...args){
+        if (!called){
+            called = true;
+            return fn(...args);   
         }
     }
 };
