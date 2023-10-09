47ee6dfaf2d09222f828efe0d86294ffe60528fb
Nirav Venkatesan <niravvenkatesan@gmail.com>
2023-10-09 23:35:50 +0000

    [LeetCode Sync] Runtime - 59 ms (45.98%), Memory - 44.3 MB (100.00%)

--- solved-problems/2797-event-emitter/solution.ts
+++ solved-problems/2797-event-emitter/solution.ts
@@ -4,33 +4,32 @@ type Subscription = {
 }
 
 class EventEmitter {
+    
+	subscribe(eventName: string, callback: Callback): Subscription {
+		
+        if (!this[eventName]) this[eventName] = []
+        this[eventName] = [...this[eventName], callback];
 
-    subscribe(eventName: string, callback: Callback): Subscription {
-        // add the callback as a method under the label of the event name
-        if (!this[eventName]) {
-            this[eventName] = [callback]
-        } else {
-            this[eventName].push(callback)
-        }
-
-        return {
-            unsubscribe: () => {
-                // remove the specific callback that was added 
-                const indexToRemove = this[eventName].findIndex(item => item === callback)
-                if (indexToRemove < 0) return
-                this[eventName].splice(indexToRemove, 1)
-            }
+		return {
+			unsubscribe: () => {
+				this[eventName].shift();
+                return undefined;
+			}
         };
-    }
+	}
 
-    emit(eventName: string, args: any[] = []): any[] {
-        if (!this[eventName]) return []
-        let result = []
-        for (const cb of this[eventName]){
-            result.push(cb(...args))
+	emit(eventName: string, args: any[] = []): any[] {
+		
+        if (!this[eventName]) return [];
+        
+        const result = []
+
+        for (const fn of this[eventName]){
+            result.push(fn(...args));
         }
-        return result
-    }
+
+        return result;
+	}
 }
 
 /**
