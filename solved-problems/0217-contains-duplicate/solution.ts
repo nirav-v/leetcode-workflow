4fa902d69db20f50dd92ba3b5ce657ec1c97f804
Nirav Venkatesan <niravvenkatesan@gmail.com>
2023-10-11 17:02:12 +0000

    [LeetCode Sync] Runtime - 79 ms (5.13%), Memory - 56.3 MB (100.00%)

--- solved-problems/0217-contains-duplicate/solution.ts
+++ solved-problems/0217-contains-duplicate/solution.ts
@@ -1,8 +1,23 @@
+
+
 function containsDuplicate(nums: number[]): boolean {
-    const numsSet = new Set()
+    // brute force: for each element, loop over remaining elements and check for duplicates
+    // O(n^2)
+    // for (let i = 0; i < nums.length; i++){
+    //     for (let j = i + 1; j < nums.length; j++){
+    //         if (nums[i] === nums[j]) return true;
+    //     }
+    // }
+    // return false;
+
+
+    // O(n)
+    // loop through, store each element in a Set, if an element is already in the set, return true
+
+    let uniqueNums = new Set();
     for (const num of nums){
-        if (numsSet.has(num)) return true
-        numsSet.add(num)
+        if (uniqueNums.has(num)) return true;
+        uniqueNums.add(num);
     }
-    return false
+    return false;
 };
