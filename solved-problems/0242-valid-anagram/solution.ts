e2770d995f629f4da51127ddd1cd62f251340a3f
Nirav Venkatesan <niravvenkatesan@gmail.com>
2023-10-12 22:10:49 +0000

    [LeetCode Sync] Runtime - 71 ms (5.19%), Memory - 45 MB (100.00%)

--- solved-problems/0242-valid-anagram/solution.ts
+++ solved-problems/0242-valid-anagram/solution.ts
@@ -1,23 +1,20 @@
 function isAnagram(s: string, t: string): boolean {
+    // keep tack of every letter in s, and how many times it occurs
+    const sChars = {}
 
+    // cannot be anagrams if different lengths
     if (s.length !== t.length) return false;
 
-    const sMap = new Map();
-    const tMap = new Map();
-
-    for (let i = 0; i < s.length; i++) {
-        const sCount = sMap.get(s[i])
-        if (sCount) sMap.set(s[i], sCount + 1)
-        else sMap.set(s[i], 1)
-
-        const tCount = tMap.get(t[i])
-        if (tCount) tMap.set(t[i], tCount + 1)
-        else tMap.set(t[i], 1)
+    for (const char of s){
+        if (!sChars[char]) sChars[char] = 1;
+        else sChars[char]++;
     }
 
-    for (const letter of sMap.keys()){
-        if (tMap.get(letter) !== sMap.get(letter)) return false;
+    for (const char of t){
+        if (!sChars[char]) return false;
+        else sChars[char]--;
     }
-    
+
     return true;
+
 };
