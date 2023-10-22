44aaa3ce0eec19ea63d642631dd47cb1199edb41
Nirav Venkatesan <niravvenkatesan@gmail.com>
2023-10-22 19:40:39 +0000

    [LeetCode Sync] Runtime - 69 ms (5.15%), Memory - 50.2 MB (100.00%)

--- solved-problems/0125-valid-palindrome/solution.ts
+++ solved-problems/0125-valid-palindrome/solution.ts
@@ -1,35 +1,53 @@
 function isPalindrome(s: string): boolean {
-    let l = 0
-    let r = s.length - 1
 
-    while (l < r){
-   
-        while (l < r && !isAlphaNumeric(s[l])) {
-            // stop checking we go out of bounds 
-            l+=1
-        }
+    //solution 1: removing spaces and replacing special chars using regex
+    // s = s.split(' ').join('');
+    // s = s.replace(/[^a-zA-Z0-9]/g, '');
+ 
+ 
+    // let i = 0
+    // let j = s.length - 1;
 
-        while (l < r && !isAlphaNumeric(s[r])) {
-            r-=1
-        }
+    // while (i <= j){
+    //     if (s[i].toLowerCase() !== s[j].toLowerCase()) return false;
+    //     else {
+    //         i++
+    //         j--
+    //     }
+    // }
+    // return true;
 
-        if (s[l].toLowerCase() !== s[r].toLowerCase()) return false;
+    // two pointers
+    // if char code not in valid alpha numeric range, skip
+    // if chars not equal while i < j, return false
 
-        l++;
-        r--;
+ 
+
+    function isAlpha(char: string): boolean {
+        if (
+            !char.match(/[^a-zA-Z0-9]/)
+        // (char.charCodeAt(0) >= 'A'.charCodeAt(0) && char.charCodeAt(0) <= 'Z'.charCodeAt(0)) || 
+        // (char.charCodeAt(0) >= 'a'.charCodeAt(0) && char.charCodeAt(0) <= 'z'.charCodeAt(0)) ||
+        // (parseInt(char) >=0 && parseInt(char) <=9)
+        ) return true;
+
+        return false;
     }
 
-    return true
-};
+    let i = 0;
+    let j = s.length - 1;
 
 
-function isAlphaNumeric(char) {
-    let code = char.charCodeAt();
-    if (!(code > 47 && code < 58) && // numeric (0-9)
-        !(code > 64 && code < 91) && // upper alpha (A-Z)
-        !(code > 96 && code < 123)) { // lower alpha (a-z)
-      return false;
+    while (i < j){
+       
+       while (!isAlpha(s[i]) && i < j) i++;
+       while (!isAlpha(s[j]) && j > i) j--;
+
+        if (s[i].toLowerCase() !== s[j].toLowerCase()) return false;
+
+        i++;
+        j--;
     }
-  
+
     return true;
 };
