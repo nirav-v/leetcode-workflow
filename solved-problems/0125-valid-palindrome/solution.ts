57bd9a212d76ea9ffd86548381e3707902cc61a5
Nirav Venkatesan <niravvenkatesan@gmail.com>
2023-11-25 17:08:52 +0000

    Sync LeetCode submission - Valid Palindrome - Runtime - 70 ms (5.00%), Memory - 50.3 MB (100.00%)

--- solved-problems/0125-valid-palindrome/solution.ts
+++ solved-problems/0125-valid-palindrome/solution.ts
@@ -1,47 +1,23 @@
 function isPalindrome(s: string): boolean {
+    const isAlphaNumeric = (str: string) =>/^[a-zA-Z0-9]+$/.test(str);
 
-    //solution 1: removing spaces and replacing special chars using regex
-    // s = s.split(' ').join('');
-    // s = s.replace(/[^a-zA-Z0-9]/g, '');
- 
- 
-    // let i = 0
-    // let j = s.length - 1;
-
-    // while (i <= j){
-    //     if (s[i].toLowerCase() !== s[j].toLowerCase()) return false;
-    //     else {
-    //         i++
-    //         j--
-    //     }
-    // }
-    // return true;
-
-    // two pointers
-    // if char code not in valid alpha numeric range, skip
-    // if chars not equal while i < j, return false
-
- 
-
-    function isAlpha(char: string): boolean {
-        if (
-            !char.match(/[^a-zA-Z0-9]/)
-        // (char.charCodeAt(0) >= 'A'.charCodeAt(0) && char.charCodeAt(0) <= 'Z'.charCodeAt(0)) || 
-        // (char.charCodeAt(0) >= 'a'.charCodeAt(0) && char.charCodeAt(0) <= 'z'.charCodeAt(0)) ||
-        // (parseInt(char) >=0 && parseInt(char) <=9)
-        ) return true;
-
-        return false;
-    }
+    if (s.length < 1) return false;
 
     let i = 0;
     let j = s.length - 1;
-
+// "A man, a plan, a canal: Panama"
+             
 
     while (i < j){
-       
-       while (!isAlpha(s[i]) && i < j) i++;
-       while (!isAlpha(s[j]) && j > i) j--;
+        while (!isAlphaNumeric(s[i]) && i < j){
+            i+=1
+
+        }
+        while (!isAlphaNumeric(s[j]) && i < j){
+            j-=1
+        }
+        // check if i and j crossed over in the inner while loops
+        // if (i >= j) break;
 
         if (s[i].toLowerCase() !== s[j].toLowerCase()) return false;
 
