bd19a03a006ee21ea02a486324bdd2126076a50b
Nirav Venkatesan <niravvenkatesan@gmail.com>
2023-11-29 03:25:41 +0000

    [LeetCode Sync] Runtime - 60 ms (44.89%), Memory - 44.8 MB (100.00%)

--- solved-problems/2761-array-reduce-transformation/solution.ts
+++ solved-problems/2761-array-reduce-transformation/solution.ts
@@ -1,9 +1,10 @@
 type Fn = (accum: number, curr: number) => number
 
 function reduce(nums: number[], fn: Fn, init: number): number {
-    let accumulator = init;
+    // apply the reduce fn to the result of the previous function call
 
-    nums.forEach(num => accumulator = fn(accumulator, num));
-
-    return accumulator;
+    for (const num of nums){
+        init = fn(init, num)
+    }
+    return init;
 };
