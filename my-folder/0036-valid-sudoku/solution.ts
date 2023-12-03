c112252a2f79d5bb9e37ec32942d5cce229cc7b7
Nirav Venkatesan <niravvenkatesan@gmail.com>
2023-12-03 01:15:32 +0000

    [LeetCode Sync] Runtime - 127 ms (6.73%), Memory - 52.1 MB (96.09%)

--- my-folder/0036-valid-sudoku/solution.ts
+++ my-folder/0036-valid-sudoku/solution.ts
@@ -1,53 +1,51 @@
 function isValidSudoku(board: string[][]): boolean {
+    // build a set for each row, check for repeats
+    // same for columns
+    // same for each cell
 
-// maintain a reference to the rows and columns we have already encountered as we loop through each cell
-// key of each row number, value of a set conatining items in that row
-    const rows = Array.from({ length: 9 }, () => new Set<string>());
-    const cols = Array.from({ length: 9 }, () => new Set<string>());
-// key of each column index, value of set conataining items in that col
+    const rows = {}
+    const columns = {}
+    const cells = {}
 
-// represent each of the nine squares by row, col (1 - 9), store a set of value in each square
-// key of [row, col] val of value 
-    const squares = {
-        '[0, 0]': new Set(),
-        '[0, 1]': new Set(),
-        '[0, 2]': new Set(),
-        '[1, 0]': new Set(),
-        '[1, 1]': new Set(),
-        '[1, 2]': new Set(),
-        '[2, 0]': new Set(),
-        '[2, 1]': new Set(),
-        '[2, 2]': new Set(),
+    for (let i = 0; i < board.length; i++){
+        rows[i] = new Set()
+        columns[i] = new Set()
+        for (let j = 0; j < board[i].length; j++){
+            cells[`${Math.floor(i/3)}, ${Math.floor(j/3)}`] = new Set()
+        }
     }
 
-console.log(rows, cols)
-    //  loop over rows
+   
+
     for (let i = 0; i < board.length; i++){
-        let rowNumber = i
-        let row = board[i];
-        // loop over cols within a row
-        for (let j = 0; j < row.length; j++){
-            let colNumber = j;
-            let cell = row[j];
-            if (cell === ".") continue;
-            if (rows[rowNumber].has(cell)) {
-                console.log("repeat row", cell, rows[rowNumber])
+        for (let j = 0; j < board[i].length; j++){
+            const cell = board[i][j];
+            if (cell === ".") continue
+            if (rows[i].has(cell)){
+                 console.log(rows, columns, cells)
+
                 return false
+            } else {
+                rows[i].add(cell)
             }
-            if (cols[colNumber].has(cell)) {
-                console.log("repeat col", cell, cols[colNumber])
-                return false;
+
+            if (columns[j].has(cell)){
+                 console.log(rows, columns, cells)
+
+             return false
+            } else {
+                columns[j].add(cell)
             }
-            if (squares[`[${Math.floor(rowNumber / 3)}, ${Math.floor(colNumber / 3)}]`].has(cell)) return false;
-                
-            rows[rowNumber].add(cell);
-            cols[colNumber].add(cell);
-            squares[`[${Math.floor(rowNumber / 3)}, ${Math.floor(colNumber / 3)}]`].add(cell)
+
+            if (cells[`${Math.floor(i/3)}, ${Math.floor(j/3)}`].has(cell)){
+                 console.log(rows, columns, cells)
+
+                 return false
+            } else cells[`${Math.floor(i/3)}, ${Math.floor(j/3)}`].add(cell)
 
         }
-                console.log(rows, cols)
     }
 
+return true;
 
-    return true;
 };
