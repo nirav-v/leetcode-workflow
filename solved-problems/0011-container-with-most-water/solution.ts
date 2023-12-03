1260579c5e10517be090a4896e3b0c83f943de77
Nirav Venkatesan <niravvenkatesan@gmail.com>
2023-12-03 17:21:07 +0000

    Sync LeetCode submission - Container With Most Water - Runtime - 1099 ms (5.00%), Memory - 83.5 MB (6.15%)

--- solved-problems/0011-container-with-most-water/solution.ts
+++ solved-problems/0011-container-with-most-water/solution.ts
@@ -1,29 +1,27 @@
-function maxArea(height: number[]): number {
-    // maxArea: smaller line * difference between both lines
-    let maxArea = 0;
+function maxArea(lines: number[]): number {
+    // initilaize max height to the area between the first and last lines (greatest width)
+    // whichever number is smaller, we move inwards to check incase it is a higher number
+    // repeat until the pointers to each line cross
 
     let i = 0;
-    let j = height.length -1;
+    let j = lines.length - 1;
+// [2,3,4,5,18,17,6]
+    let maxArea = 0;
 
     while (i < j){
+        const height = Math.min(lines[j], lines[i]);
         const width = j - i;
-        const minHeight = Math.min(height[i], height[j]);
-        const area = width * minHeight;
-        maxArea = Math.max(area, maxArea)
-        
-        if (height[i] < height[j]){
-            i+=1
-        } else if (height[j] < height[i]){
-            j-=1
-        } else {
-            j-=1
-        }
-
+        const area = width * height;
+        console.log(area)
+        maxArea = Math.max(area, maxArea);
+        if (lines[i] < lines[j]){
+            i++
+        } else j--;
     }
-     
-        
-    
+
     return maxArea;
- 
+    
+
+
 
 };
