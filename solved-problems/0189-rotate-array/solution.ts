b7bb663ea1228dd798aeeb01a2996782c25cf64c
Nirav Venkatesan <niravvenkatesan@gmail.com>
2023-12-06 01:54:03 +0000

    [LeetCode Sync] Runtime - 865 ms (5.32%), Memory - 54.4 MB (100.00%)

--- solved-problems/0189-rotate-array/solution.ts
+++ solved-problems/0189-rotate-array/solution.ts
@@ -2,9 +2,26 @@
  Do not return anything, modify nums in-place instead.
  */
 function rotate(nums: number[], k: number): void {
-    while (k >0){
-        const popped = nums.pop()
-        nums.unshift(popped)
-        k--
+    // -------
+      // for (let i = 0; i < k; i++){
+      //     const removed = nums.pop();
+      //     nums.unshift(removed)
+      // }
+    // ----------
+
+    // swap first num with last num
+    // swap last num with prev num 
+    let i = 0;
+    while (i < k){
+      let last = nums[nums.length -1];
+      nums.unshift(last)
+      nums.pop();
+      i++;
     }
+
 };
+
+// [1, 2]
+// [2, 1]
+// [1, 2]
+// 2, 1
