a34cf17a1f5da42f51af1bb972d16e4ec38936f2
Nirav Venkatesan <niravvenkatesan@gmail.com>
2023-12-11 22:02:03 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock - Runtime - 76 ms (5.02%), Memory - 51.5 MB (100.00%)

--- solved-problems/0121-best-time-to-buy-and-sell-stock/solution.ts
+++ solved-problems/0121-best-time-to-buy-and-sell-stock/solution.ts
@@ -1,22 +1,26 @@
-
 // [7,1,5,3,6,4]
-// greatest value of day 2 - day 1
+
 function maxProfit(prices: number[]): number {
     
-    let result = 0;
+    let result = 0
+    
+    let day1 = 0;
+    let day2 = 1;
 
-    let minPrice = Infinity;
 
-    for (const price of prices){
-        if (price < minPrice){
-            minPrice = price
-        } else if (price - minPrice > result) {
-            result = price - minPrice
-        };
+    while (day2 < prices.length){
+        // ensure that we buy at a lower price than we sell
+        while (prices[day1] > prices[day2]){
+            day1 = day2;
+            // if not already at the last index, move day2 forward
+            if (day2 < prices.length -1) day2++
+        }
 
+        result = Math.max(result, (prices[day2] - prices[day1]))
+
+        day2++;
     }
 
     return result;
+
 };
-
-
