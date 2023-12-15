c21e937ea37f56bdeaecb0759e5c7feb2a685680
Nirav Venkatesan <niravvenkatesan@gmail.com>
2023-12-15 21:11:54 +0000

    [LeetCode Sync] Runtime - 206 ms (19.38%), Memory - 51.2 MB (100.00%)

--- solved-problems/0567-permutation-in-string/solution.ts
+++ solved-problems/0567-permutation-in-string/solution.ts
@@ -1,35 +1,39 @@
 function checkInclusion(s1: string, s2: string): boolean {
-    // permutation if: same length, same counts of each char
-    const s1Counts = new Map()
-    const windowCounts = new Map()
+    let i = 0;
+    let j = 0;
+
+    const s1CharCounts = {}
 
     for (const char of s1){
-        if (s1Counts.get(char)) s1Counts.set(char, s1Counts.get(char) + 1)
-        else s1Counts.set(char, 1)
+        if (s1CharCounts[char]) s1CharCounts[char] +=1
+        else s1CharCounts[char] = 1
     }
 
-    let l = 0
+    const counts = {}
 
-    for (let r = 0; r < s2.length; r++){
-        // resize window from left if needed
-        if ((r - l + 1) > s1.length){
-            windowCounts.set(s2[l], windowCounts.get(s2[l]) - 1)
-            l++
+
+    for (let j = 0; j < s2.length; j++){
+
+        if (counts[s2[j]]) counts[s2[j]]+=1
+        else counts[s2[j]] = 1;
+
+        // shrink window if we go past s1 length
+        if ((j - i + 1) > s1.length){
+            counts[s2[i]]--;
+            i++;
         }
 
-        // add new right char to windowCounts
-        if (windowCounts.get(s2[r])) windowCounts.set(s2[r], windowCounts.get(s2[r]) + 1)
-        else windowCounts.set(s2[r], 1)
 
-        // check if all chars match in count between s1 and s2
-        let matches = 0
-        windowCounts.forEach((value, key)=> {
-            if (windowCounts.get(key) === s1Counts.get(key)) matches++
-        })
+        // console.log(s1CharCounts, counts)
 
-        if (matches === s1Counts.size) return true
+        // compare the char counts of current substring to s1
+        let matches = 0;
+        for (const key of Object.keys(s1CharCounts)){
+            if (s1CharCounts[key] === counts[key]) matches+=1;
+        }
+        // console.log(s1.length, matches)
+        if (matches === Object.keys(s1CharCounts).length) return true;
     }
 
-    return false
-
+    return false;
 };
