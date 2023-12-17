4dcc193e56583ac1440df67a99419958313ff304
Nirav Venkatesan <niravvenkatesan@gmail.com>
2023-12-17 19:43:38 +0000

    Sync LeetCode submission - Longest Repeating Character Replacement - Runtime - 947 ms (5.31%), Memory - 51.9 MB (99.82%)

--- solved-problems/0424-longest-repeating-character-replacement/solution.ts
+++ solved-problems/0424-longest-repeating-character-replacement/solution.ts
@@ -1,30 +1,24 @@
 function characterReplacement(s: string, k: number): number {
-    
-    // store the count of each char in a hash map
-    // if the count least frequent chars (length of substring - count of most frequent char) is more than k possbile changes, decrement that char count in the object and move the substring forward
-// calculate the max length
+    let res = 0;
+    let i = 0;
+    let counts = {}
+    for (let j = 0; j < s.length; j++){
+        let char = s[j]
+        if (counts[char]) counts[char]++;
+        else counts[char] = 1;
+        
+        const countValues: number[] = Object.values(counts);
+        const mostFrequentCount = Math.max(...countValues);
+        console.log(counts)
+        while ((j - i + 1) - mostFrequentCount > k){
+            counts[s[i]] -=1;
+            i++;
+        }
 
-let res = 0;
-const counts = {}
-let i = 0;
+        res = Math.max(res, (j - i + 1));
 
-for (let j = 0; j < s.length; j++){
-
-    if (counts[s[j]]) counts[s[j]] += 1;
-    else counts[s[j]] = 1;
-
-    
-    const charValues: number[] = Object.values(counts)
-    const mostFrequentCharCount = Math.max(...charValues)
-
-
-    if ( (j - i + 1) - mostFrequentCharCount > k){
-        counts[s[i]]-=1
-        i++;
     }
 
-    res=Math.max(res,  j - i + 1)
-}   
-
-return res;
+    return res;
+// AABABBA
 };
