670bfdfde757a7888da2e0315250eea2f6e7aef4
Nirav Venkatesan <niravvenkatesan@gmail.com>
2024-02-04 21:40:22 +0000

    [LeetCode Sync] Runtime - 79 ms (5.07%), Memory - 59.9 MB (68.65%)

--- solved-problems/0121-best-time-to-buy-and-sell-stock/solution.ts
+++ solved-problems/0121-best-time-to-buy-and-sell-stock/solution.ts
@@ -1,26 +1,19 @@
-// [7,1,5,3,6,4]
 
 function maxProfit(prices: number[]): number {
-    
-    let result = 0
-    
-    let day1 = 0;
-    let day2 = 1;
 
+    let i = 0;
+    let buyingPrice = prices[0]
+    let maxProfit = 0;
 
-    while (day2 < prices.length){
-        // ensure that we buy at a lower price than we sell
-        while (prices[day1] > prices[day2]){
-            day1 = day2;
-            // if not already at the last index, move day2 forward
-            if (day2 < prices.length -1) day2++
+    for (let i = 1; i < prices.length; i++){
+        // dont bother calculating if our buy price is higher than the sell price
+        if (prices[i] < buyingPrice){
+            buyingPrice = prices[i];
+        } else {
+            maxProfit = Math.max(maxProfit, prices[i] - buyingPrice)
         }
-
-        result = Math.max(result, (prices[day2] - prices[day1]))
-
-        day2++;
     }
-
-    return result;
+    
+    return maxProfit;
 
 };
