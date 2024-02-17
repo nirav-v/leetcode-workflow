781d9dd8f201734501bb219f4e15f654be8ba5eb
Nirav Venkatesan <niravvenkatesan@gmail.com>
2024-02-17 21:41:51 +0000

    [LeetCode Sync] Runtime - 65 ms (18.26%), Memory - 52 MB (90.87%)

--- solved-problems/0001-two-sum/solution.ts
+++ solved-problems/0001-two-sum/solution.ts
@@ -1,22 +1,14 @@
 function twoSum(nums: number[], target: number): number[] {
-   
-    // map key - number, value - index
-    const numsMap = {}
-
-    for (let i = 0; i < nums.length; i++) {
-        const num = nums[i]
-        const complement = target - num
-     
-        // check that the map has the complement, if it does return our current num index and the complement index
-        if (numsMap[complement.toString()] !== undefined) {
-            return [i, numsMap[complement]]
+    let pastNums = {}
+    for (let i = 0; i < nums.length; i++){
+        
+        const complement = target - nums[i];
+        if (pastNums[complement] !== undefined){
+            return [pastNums[complement.toString()], i]
+        } else {
+            pastNums[nums[i]] = i;
         }
 
-        // store num: index in our map
-        numsMap[num] = i
+
     }
-
-    // default, should never reach here assuming there is at least one valid combination in the array
-    return [0, 0]
-
 };
