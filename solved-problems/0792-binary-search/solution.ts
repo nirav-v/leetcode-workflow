f985caed5b66208342721c04a41c02d8718e01e0
Nirav Venkatesan <niravvenkatesan@gmail.com>
2024-02-18 19:38:19 +0000

    Sync LeetCode submission - Binary Search - Runtime - 53 ms (5.05%), Memory - 53.8 MB (59.62%)

--- solved-problems/0792-binary-search/solution.ts
+++ solved-problems/0792-binary-search/solution.ts
@@ -1,15 +1,22 @@
 function search(nums: number[], target: number): number {
     
-    let l = 0;
-    let r = nums.length - 1;
 
-    while (l <= r){
-        const mid = Math.floor((r + l) / 2);
-        if (nums[mid] < target) l = mid + 1;
-        else if (nums[mid] > target) r = mid - 1;
-        else return mid;
+    let left = 0;
+    let right = nums.length - 1
+ 
+
+
+    while (left <= right){
+        let mid = Math.floor((right + left)/2)
+        if (nums[mid] < target) {
+            left = mid + 1;
+        } else if (nums[mid] > target){
+            right = mid - 1;
+        } else {
+            return mid;
+        }
     }
 
-    return -1; 
+    return -1
 
 };
