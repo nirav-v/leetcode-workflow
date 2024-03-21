7734ad3ad32b94a5645a0d9d1b12b1938a283841
Nirav Venkatesan <niravvenkatesan@gmail.com>
2024-03-21 19:00:13 +0000

    Sync LeetCode submission - Valid Palindrome - Runtime - 55 ms (5.00%), Memory - 52.5 MB (86.20%)

--- solved-problems/0125-valid-palindrome/solution.ts
+++ solved-problems/0125-valid-palindrome/solution.ts
@@ -1,36 +1,35 @@
 function isPalindrome(s: string): boolean {
+    let l = 0
+    let r = s.length - 1
 
-    let i = 0;
-    let j = s.length - 1;
-
-    function isAlphaNumeric(char) {
-        const code = char.charCodeAt(0);
-        if (!(code > 47 && code < 58) && // numeric (0-9)
-            !(code > 64 && code < 91) && // upper alpha (A-Z)
-            !(code > 96 && code < 123)) { // lower alpha (a-z)
-            return false;
+    while (l < r){
+   
+        while (l < r && !isAlphaNumeric(s[l])) {
+            // stop checking we go out of bounds 
+            l+=1
         }
-        return true;
-    };
 
-    while (i < j) {
+        while (l < r && !isAlphaNumeric(s[r])) {
+            r-=1
+        }
 
-        while (s[i] && !isAlphaNumeric(s[i])) {
-            i++;
-        };
+        if (s[l].toLowerCase() !== s[r].toLowerCase()) return false;
 
-        while (s[j] && !isAlphaNumeric(s[j])) {
-            j--;
-        };
-
-        if (!s[i] || !s[j]) break;
-
-        if (s[i].toLowerCase() !== s[j].toLowerCase()) return false;
-
-        i++;
-        j--;
+        l++;
+        r--;
     }
 
-    return true;
+    return true
+};
 
-}; 
+
+function isAlphaNumeric(char) {
+    let code = char.charCodeAt();
+    if (!(code > 47 && code < 58) && // numeric (0-9)
+        !(code > 64 && code < 91) && // upper alpha (A-Z)
+        !(code > 96 && code < 123)) { // lower alpha (a-z)
+      return false;
+    }
+  
+    return true;
+};
