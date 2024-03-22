72f54df72d29aba2522d1c9d414ad8b42619dc69
Nirav Venkatesan <niravvenkatesan@gmail.com>
2024-03-22 17:29:26 +0000

    [LeetCode Sync] Runtime - 62 ms (32.74%), Memory - 52.3 MB (53.92%)

--- solved-problems/1894-merge-strings-alternately/solution.ts
+++ solved-problems/1894-merge-strings-alternately/solution.ts
@@ -1,19 +1,24 @@
 function mergeAlternately(word1: string, word2: string): string {
-    let result = ""
+    // pushing to array more effiecient than appending to string (recreates new string in memory every time)
+    let result = []
 
     let i = 0
-    let j = 0
+    let j = 0;
 
+    // alternate adding on chars staring with word1
     while (i < word1.length || j < word2.length) {
         if (i < word1.length) {
-            result += word1[i]
+            result.push(word1[i]);
             i++
         }
-        if (j < word2.length) {
-            result += word2[j]
+
+        if (j < word2.length){
+            result.push(word2[j]);
             j++
         }
     }
 
-    return result
+    return result.join("")
 };
+
+// "ab" "pqrs"
