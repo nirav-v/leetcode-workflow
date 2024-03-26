a035410a8355fc913960a632424c7e57f6ac9453
Nirav Venkatesan <niravvenkatesan@gmail.com>
2024-03-26 18:32:07 +0000

    [LeetCode Sync] Runtime - 58 ms (5.24%), Memory - 51.7 MB (54.02%)

--- solved-problems/0088-merge-sorted-array/solution.ts
+++ solved-problems/0088-merge-sorted-array/solution.ts
@@ -1,34 +1,28 @@
 /**
  Do not return anything, modify nums1 in-place instead.
  */
-function merge(nums1: number[], m: number, nums2: number[], n: number): void {    
-    // for (let i = 0; i < n; i++){
-    //     nums1[m + i] = nums2[i]
-    // }
-    
-    // nums1.sort(((a,b) => a -b))
+function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+    let i = m - 1;
+    let j = n - 1;
 
-    // start comparing the last  of both arrays, put the greater value into the end of nums1
-    let i = m - 1
-    let j = nums2.length - 1
+    while (j >= 0) {
+        let replacementIndex = m + n - 1
 
-    while (j >= 0){
-        // compare i and j, greater one gets put into nums1
-        if (nums1[i] > nums2[j]){
-            nums1[m + n - 1] = nums1[i]
-            i--;
-        } else {
-           nums1[m + n - 1] = nums2[j]
-           j--
+        if (nums1[i] > nums2[j]) {
+            nums1[replacementIndex] = nums1[i]
+            i--
         }
-        n--
+        else {
+            nums1[replacementIndex] = nums2[j]
+            j--
+        }
+
+        n--;
     }
-
-//    i 
-// [1,2,2,3,5,6]
-
-// j
-// [2,5,6]
-
 };
 
+//        i        r
+// [1, 2, 3, 0, 0, 0]
+
+//        j
+// [2, 5, 6]
