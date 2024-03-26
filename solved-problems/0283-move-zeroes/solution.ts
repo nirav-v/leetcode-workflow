5d24c48a848dc6e0ebf18e533b1c6670743245ab
Nirav Venkatesan <niravvenkatesan@gmail.com>
2024-03-26 18:52:00 +0000

    [LeetCode Sync] Runtime - 79 ms (5.80%), Memory - 55.2 MB (73.85%)

--- solved-problems/0283-move-zeroes/solution.ts
+++ solved-problems/0283-move-zeroes/solution.ts
@@ -2,12 +2,28 @@
  Do not return anything, modify nums in-place instead.
  */
 function moveZeroes(nums: number[]): void {
-    // [3,1,0,0,12]
-    let l = 0
-    for (let r in nums){
-        if (nums[r] !== 0){
-            [nums[l], nums[r]] = [nums[r], nums[l]]
-            l++
-        } 
+    
+    let i =0
+    let j = 1
+
+    while (j < nums.length){
+        if (nums[i] === 0){
+            // make sure we move j up to a non-zero num
+            while (nums[j] === 0) j++;
+            // break early if j went out of bounds
+            if (nums[j] === undefined) break
+            // swap i and j nums
+            let temp = nums[i];
+            nums[i] = nums[j]
+            nums[j] = temp
+        }
+        i++
+        j++
     }
 };
+
+//    i j
+// [1,0,0,3,12]
+
+// [1,3,12,0,0]
+
