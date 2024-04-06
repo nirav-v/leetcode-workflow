b718fdd60088b3933874cbbf114d0abedfe1022f
Nirav Venkatesan <niravvenkatesan@gmail.com>
2024-04-06 20:00:38 +0000

    [LeetCode Sync] Runtime - 82 ms (5.05%), Memory - 57.7 MB (85.53%)

--- solved-problems/0344-reverse-string/solution.ts
+++ solved-problems/0344-reverse-string/solution.ts
@@ -2,18 +2,15 @@
  Do not return anything, modify s in-place instead.
  */
 function reverseString(s: string[]): void {
-    
     let l = 0;
     let r = s.length - 1
 
-    while(l < r){
-        const temp = s[l]
-        s[l] = s[r];
-        s[r] = temp;
+    while (l < r){
+        let temp = s[l];
+        s[l] = s[r]
+        s[r] = temp
 
         l++
         r--
     }
 };
-
-
