34a4318ce29ebfb1e7c9128098fdb03bcf13c03e
Nirav Venkatesan <niravvenkatesan@gmail.com>
2024-04-14 21:23:10 +0000

    [LeetCode Sync] Runtime - 148 ms (24.42%), Memory - 56.9 MB (26.38%)

--- my-folder/0424-longest-repeating-character-replacement/solution.ts
+++ my-folder/0424-longest-repeating-character-replacement/solution.ts
@@ -1,24 +1,40 @@
 function characterReplacement(s: string, k: number): number {
-    let res = 0;
-    let i = 0;
-    let counts = {}
-    for (let j = 0; j < s.length; j++){
-        let char = s[j]
-        if (counts[char]) counts[char]++;
-        else counts[char] = 1;
-        
-        const countValues: number[] = Object.values(counts);
-        const mostFrequentCount = Math.max(...countValues);
-        console.log(counts)
-        while ((j - i + 1) - mostFrequentCount > k){
-            counts[s[i]] -=1;
-            i++;
-        }
 
-        res = Math.max(res, (j - i + 1));
+    let longest = 0
+
+    let charCounts = {}
+
+    let l = 0
+    let r = 1
+
+    for (let r = 0; r < s.length; r++) {
+        // add count to map
+        if (charCounts[s[r]]) charCounts[s[r]] += 1
+        else charCounts[s[r]] = 1
+
+        // get most frequent char from window, subtract from window size, check if result is within allowed replacements
+        let mostFreuqent = getMaxCharCount(charCounts)
+        let windowSize = r - l + 1
+
+        while (windowSize - mostFreuqent > k) {
+            // decrement left char in map
+            charCounts[s[l]] -= 1
+            mostFreuqent = getMaxCharCount(charCounts)
+            // shrink window
+            l++
+            windowSize = r - l + 1
+        }
+        longest = Math.max(longest, r - l + 1)
 
     }
 
-    return res;
-// AABABBA
+    return longest
 };
+
+function getMaxCharCount(obj) {
+    let maxCount = 0
+    for (const key of Object.keys(obj)) {
+        maxCount = Math.max(maxCount, obj[key])
+    }
+    return maxCount
+}
