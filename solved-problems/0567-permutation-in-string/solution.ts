f4707cf1d41470fa216d6026c8bdc5b520c090ed
Nirav Venkatesan <niravvenkatesan@gmail.com>
2024-04-26 22:29:13 +0000

    [LeetCode Sync] Runtime - 135 ms (22.47%), Memory - 57.5 MB (47.84%)

--- solved-problems/0567-permutation-in-string/solution.ts
+++ solved-problems/0567-permutation-in-string/solution.ts
@@ -1,39 +1,35 @@
 function checkInclusion(s1: string, s2: string): boolean {
-    let i = 0;
-    let j = 0;
-
-    const s1CharCounts = {}
+    // permutation if: same length, same counts of each char
+    const s1Counts = new Map()
+    const windowCounts = new Map()
 
     for (const char of s1){
-        if (s1CharCounts[char]) s1CharCounts[char] +=1
-        else s1CharCounts[char] = 1
+        if (s1Counts.get(char)) s1Counts.set(char, s1Counts.get(char) + 1)
+        else s1Counts.set(char, 1)
     }
 
-    const counts = {}
+    let l = 0
 
-
-    for (let j = 0; j < s2.length; j++){
-
-        if (counts[s2[j]]) counts[s2[j]]+=1
-        else counts[s2[j]] = 1;
-
-        // shrink window if we go past s1 length
-        if ((j - i + 1) > s1.length){
-            counts[s2[i]]--;
-            i++;
+    for (let r = 0; r < s2.length; r++){
+        // resize window from left if needed
+        if ((r - l + 1) > s1.length){
+            windowCounts.set(s2[l], windowCounts.get(s2[l]) - 1)
+            l++
         }
 
+        // add new right char to windowCounts
+        if (windowCounts.get(s2[r])) windowCounts.set(s2[r], windowCounts.get(s2[r]) + 1)
+        else windowCounts.set(s2[r], 1)
 
-        // console.log(s1CharCounts, counts)
+        // check if all chars match in count between s1 and s2
+        let matches = 0
+        windowCounts.forEach((value, key)=> {
+            if (windowCounts.get(key) === s1Counts.get(key)) matches++
+        })
 
-        // compare the char counts of current substring to s1
-        let matches = 0;
-        for (const key of Object.keys(s1CharCounts)){
-            if (s1CharCounts[key] === counts[key]) matches+=1;
-        }
-        // console.log(s1.length, matches)
-        if (matches === Object.keys(s1CharCounts).length) return true;
+        if (matches === s1Counts.size) return true
     }
 
-    return false;
+    return false
+
 };
