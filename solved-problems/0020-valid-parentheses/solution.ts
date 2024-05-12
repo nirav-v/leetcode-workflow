4f199185b3e631ddc5c55c17c1556cf4c6cefad3
Nirav Venkatesan <niravvenkatesan@gmail.com>
2024-05-12 18:42:24 +0000

    [LeetCode Sync] Runtime - 50 ms (5.04%), Memory - 52.1 MB (92.23%)

--- solved-problems/0020-valid-parentheses/solution.ts
+++ solved-problems/0020-valid-parentheses/solution.ts
@@ -1,26 +1,22 @@
-
-// ([])[]{}
 function isValid(s: string): boolean {
-    
-    const openingBrackets = new Set(['[', '(', '{'])
+    // push opening brackets to stack
+    // if closing bracket, pop from stack and check that the popped value is the corresponding opening bracket
 
     const bracketsMap = {
-        ')': '(',
-        ']': '[',
-        '}': '{',
+        "(": ")",
+        "[": "]",
+        "{": "}"
     }
 
-   const stack = []
+    const stack = []
 
     for (const bracket of s){
-        if (openingBrackets.has(bracket)){
-            stack.push(bracket)
-        } else {
-            const poppedValue = stack.pop();
-            if (poppedValue !== bracketsMap[bracket]) return false;
+        if (bracketsMap[bracket]) stack.push(bracket) // opening bracket
+        else { //closing bracket
+            const poppedVal = stack.pop()
+            if (bracketsMap[poppedVal] !== bracket) return false
         }
     }
 
-    return stack.length === 0;
-
+    return stack.length === 0
 };
