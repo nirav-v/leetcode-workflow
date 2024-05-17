b8a2ff5c760353e8db7ded3e5d7b1deccaca7ae5
Nirav Venkatesan <niravvenkatesan@gmail.com>
2024-05-17 18:57:37 +0000

    [LeetCode Sync] Runtime - 56 ms (5.01%), Memory - 51.6 MB (69.40%)

--- my-folder/0088-merge-sorted-array/solution.ts
+++ my-folder/0088-merge-sorted-array/solution.ts
@@ -2,27 +2,24 @@
  Do not return anything, modify nums1 in-place instead.
  */
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-    let i = m - 1;
-    let j = n - 1;
 
-    while (j >= 0) {
-        let replacementIndex = m + n - 1
+    let i = m -1
+    let j = n -1
+    let replacementIndex = m + n - 1
 
-        if (nums1[i] > nums2[j]) {
+    while (j >= 0){
+        if (nums1[i] > nums2[j]){
             nums1[replacementIndex] = nums1[i]
             i--
-        }
-        else {
+        } else {
             nums1[replacementIndex] = nums2[j]
             j--
         }
-
-        n--;
+        replacementIndex--
     }
 };
 
-//        i        r
-// [1, 2, 3, 0, 0, 0]
 
-//        j
-// [2, 5, 6]
+
+// [1,2,2,3,5,6]
+// [2,5,6]
