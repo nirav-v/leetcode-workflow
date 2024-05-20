dcb8716df692b9cc0c2f38396d031bdca4e34860
Nirav Venkatesan <niravvenkatesan@gmail.com>
2024-05-20 21:47:32 +0000

    Sync LeetCode submission - Two Sum II - Input Array Is Sorted - Runtime - 63 ms (17.71%), Memory - 51.4 MB (99.61%)

--- solved-problems/0167-two-sum-ii---input-array-is-sorted/solution.ts
+++ solved-problems/0167-two-sum-ii---input-array-is-sorted/solution.ts
@@ -1,16 +1,15 @@
 function twoSum(numbers: number[], target: number): number[] {
-    // [2,7,11,15]
-    // [2,3,4]
-    let i = 0;
-    let j = numbers.length - 1
+    let l = 0
+    let r = numbers.length - 1
 
-    while (i < j){
-          const sum = numbers[i] + numbers[j]
-      
-            if  (sum > target) j-= 1;
-            else if (sum < target) i+=1;
-            else  return [i + 1, j + 1];
-        
+    while (l < r){
+        const sum = numbers[l] + numbers[r]
+        if (sum < target){
+            l++
+        } else if (sum > target){
+            r--
+        } else {
+            return [l + 1, r + 1]
+        }
     }
-
 };
