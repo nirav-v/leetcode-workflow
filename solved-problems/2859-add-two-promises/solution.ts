ad6c6354e0e2952b778f3b3130519da8a5d2d187
Nirav Venkatesan <niravvenkatesan@gmail.com>
2024-05-21 00:24:35 +0000

    [LeetCode Sync] Runtime - 71 ms (34.16%), Memory - 51.2 MB (97.88%)

--- solved-problems/2859-add-two-promises/solution.ts
+++ solved-problems/2859-add-two-promises/solution.ts
@@ -1,11 +1,10 @@
 type P = Promise<number>
 
 async function addTwoPromises(promise1: P, promise2: P): P {
-    
-    const result = await Promise.all([promise1, promise2])    
-    
-    return new Promise((res, rej) => res(result[0] + result[1]));
-    
+    const num1 = await promise1
+    const num2 = await promise2
+
+    return new Promise((res, rej) => res(num1 + num2))
 };
 
 /**
