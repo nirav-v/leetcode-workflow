65cc5bbb3e90a01bcb74d74834cfe85114c6ccd9
Nirav Venkatesan <niravvenkatesan@gmail.com>
2025-01-04 17:34:48 +0000

    [LeetCode Sync] Runtime - 11 ms (76.45%), Memory - 64.2 MB (32.66%)

--- my-folder/0217-contains-duplicate/solution.ts
+++ my-folder/0217-contains-duplicate/solution.ts
@@ -1,11 +1,8 @@
 function containsDuplicate(nums: number[]): boolean {
-    
-    const numsSet = new Set();
-
+    const numsSet = new Set()
     for (const num of nums){
-        if (numsSet.has(num)) return true;
+        if (numsSet.has(num)) return true
         numsSet.add(num)
     }
-
-    return false;
+    return false
 };
